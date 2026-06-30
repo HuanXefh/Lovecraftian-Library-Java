b7558d19f6ea0b0f6b75827c8afee43e876b594b
HuanXefh <huanxefh@gmail.com>
2026-07-01 01:45:20 +0800

    Update bundleGen.py

--- bundleGen.py
+++ bundleGen.py
@@ -1,23 +1,26 @@
 import pandas as PANDAS
 
 
-BUNDLE_PATH_TEST = "H:\\Dropbox\\_gm_mdt\\lovec\\mabo-lovecraftian-library\\"
-BUNDLE_PATH_LOVEC = "H:\\Dropbox\\_gm_mdt\\lovec\\mabo-lovecraftian-library\\assets\\bundles\\"
-BUNDLE_PATH_LOVECLAB = "H:\\Dropbox\\_gm_mdt\\lovec\\mabo-lovecraftian-laboratory\\bundles\\"
-BUNDLE_PATH_PROJREIND = "H:\\Dropbox\\_gm_mdt\\lovec\\mabo-project-reindustrialization\\bundles\\"
-BUNDLE_PATH_SERP2 = "H:\\Dropbox\\_gm_mdt\\lovec\\mabo-serpulo-squared\\bundles\\"
-BUNDLE_PATH_FCELL = "H:\\Dropbox\\_gm_mdt\\lovec\\mabo-fluid-cells\\bundles\\"
 BUNDLE_FILE_NAME = "lovec-bundle-gen.xlsx"
+BUNDLE_PATH_DICT = {
+    "lovec": "H:\\Dropbox\\_gm_mdt\\lovec\\mabo-lovecraftian-library\\assets\\bundles\\",
+    "loveclab": "H:\\Dropbox\\_gm_mdt\\lovec\\mabo-lovecraftian-laboratory\\bundles\\",
+    "projreind": "H:\\Dropbox\\_gm_mdt\\lovec\\mabo-project-reindustrialization\\bundles\\",
+    "serp2": "H:\\Dropbox\\_gm_mdt\\lovec\\mabo-serpulo-squared\\bundles\\",
+    "fcell": "H:\\Dropbox\\_gm_mdt\\lovec\\mabo-fluid-cells\\bundles\\",
+}
+BUNDLE_PATH_TEST = "H:\\Dropbox\\_gm_mdt\\lovec\\mabo-lovecraftian-library\\"
 LANG_SUFFIX_DICT = {
     "EN": "",
+    #"RU": "_ru",
     "CN": "_zh_CN",
-    "CN-TW": "_zh_TW",
-    "JP": "_ja",
-    "KO": "_ko",
+    #"CN-TW": "_zh_TW",
+    #"JP": "_ja",
+    #"KO": "_ko",
 }
 
 
-def build(nmMod="lovec", dirTg=BUNDLE_PATH_TEST, lang="EN"):
+def buildFile(nmMod="lovec", dirTg=BUNDLE_PATH_TEST, lang="EN"):
     path = dirTg + "bundle" + LANG_SUFFIX_DICT[lang] + ".properties"
     fi = open(path, mode="w", encoding="utf-8")
     data = PANDAS.read_excel(BUNDLE_FILE_NAME, sheet_name=nmMod)
@@ -25,13 +28,10 @@ def build(nmMod="lovec", dirTg=BUNDLE_PATH_TEST, lang="EN"):
     fi.close()
 
 
-build("lovec", BUNDLE_PATH_LOVEC, "EN")
-build("lovec", BUNDLE_PATH_LOVEC, "CN")
-build("loveclab", BUNDLE_PATH_LOVECLAB, "EN")
-build("loveclab", BUNDLE_PATH_LOVECLAB, "CN")
-build("projreind", BUNDLE_PATH_PROJREIND, "EN")
-build("projreind", BUNDLE_PATH_PROJREIND, "CN")
-build("serp2", BUNDLE_PATH_SERP2, "EN")
-build("serp2", BUNDLE_PATH_SERP2, "CN")
-build("fcell", BUNDLE_PATH_FCELL, "EN")
-build("fcell", BUNDLE_PATH_FCELL, "CN")
+def build():
+    for nmMod, dirTg in BUNDLE_PATH_DICT.items():
+        for lang in LANG_SUFFIX_DICT:
+            buildFile(nmMod, dirTg, lang)
+
+
+build()
